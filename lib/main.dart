b9e8f00d1d52342e73126c6c96f4f25d625fb846
NimasSeptiandini <septyandini921@gmail.com>
2025-09-30 13:01:25 +0700

    Praktikum 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'basic_widget/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -104,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many timesssss:'),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
