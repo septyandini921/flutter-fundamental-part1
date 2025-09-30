2a88ad301884a55d81c043acf6165dda60550312
NimasSeptiandini <septyandini921@gmail.com>
2025-09-30 21:21:33 +0700

    Langkah 3 Praktikum 5

---
+++ lib/basic_widget/fab_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FabWidget extends StatelessWidget {
+  const FabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // aksi ketika tombol ditekan
+            debugPrint("FAB ditekan!");
+          },
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
